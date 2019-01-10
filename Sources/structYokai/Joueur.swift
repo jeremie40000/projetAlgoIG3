8660f5605282c3547e80f2a315493bc85c959355
Alexis ANDRE <alexis.andre@igtp4-mac02.isim.intra>
2019-01-10 15:12:46 +0100

    modif

--- Sources/structYokai/Joueur.swift
+++ Sources/structYokai/Joueur.swift
@@ -21,10 +21,12 @@ public struct Joueur : JoueurProtocol{
 		num = Num
 		nom = NomJ
 		if (Num != 1 || Num != 2) {
+			print("erreur 1")
 			throw JoueurError.MauvaisNum
 		}
 		res = Reserve()
 		if !res.ReserveEstVide() {
+			print("erreur 2")
 			throw JoueurError.ReserveNonVide
 		}
 		mainJ = MainJoueur()
